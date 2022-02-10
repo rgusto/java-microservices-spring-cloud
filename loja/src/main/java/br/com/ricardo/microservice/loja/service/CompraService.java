40eeb978135f3e018bfc03ad7c4d59e3667cd96f
Ricardo Moreira <rgusto@gmail.com>
2022-02-10 16:29:09 -0300

    Distributed Tracing e Spring Sleuth

--- loja/src/main/java/br/com/ricardo/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/ricardo/microservice/loja/service/CompraService.java
@@ -1,5 +1,8 @@
 package br.com.ricardo.microservice.loja.service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,13 +10,18 @@ import br.com.ricardo.microservice.loja.client.FornecedorClient;
 import br.com.ricardo.microservice.loja.controller.dto.CompraDTO;
 import br.com.ricardo.microservice.loja.controller.dto.InfoFornecedorDTO;
 
+
 @Service
 public class CompraService {
 
 	@Autowired
 	private FornecedorClient fornecedorClient;
-	
+
+	private Logger logger = Logger.getLogger(CompraService.class.getName());
+
 	public void realizarCompra(CompraDTO compra) {
+		
+		logger.log(Level.INFO, "Buscando informações do Fornecedor");
 		
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		System.out.println(info.getEndereco());
